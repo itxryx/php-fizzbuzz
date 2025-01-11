c582830201d4d7ce2c58274bbc2421f2dc233882
ito ryo <electrooculogram@gmail.com>
2025-01-11 20:38:29 +0900

    add tests (red)

---
+++ tests/Core/NumberConverterTest.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Itxryx\FizzBuzz\Tests\Core;
+
+use Itxryx\FizzBuzz\Core\NumberConverter;
+use PHPUnit\Framework\TestCase;
+
+class NumberConverterTest extends TestCase
+{
+    public function testConvert()
+    {
+        $fizzbuzz = new NumberConverter();
+        $this->assertEquals('1', $fizzbuzz->convert(1));
+    }
+}
