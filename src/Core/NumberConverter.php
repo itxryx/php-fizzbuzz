df75c86527168cbdd6b397b135b44dd047102fb8
ito ryo <electrooculogram@gmail.com>
2025-01-11 20:58:26 +0900

    update method (green)

--- src/Core/NumberConverter.php
+++ src/Core/NumberConverter.php
@@ -6,7 +6,11 @@ namespace Itxryx\FizzBuzz\Core;
 class NumberConverter {
     public function convert(int $n): string {
         if ($n % 3 === 0) {
-            return "Fizz";
+            if ($n % 5 === 0) {
+                return "FizzBuzz";
+            } else {
+                return "Fizz";
+            }
         } else if ($n % 5 === 0) {
             return "Buzz";
         } else {
