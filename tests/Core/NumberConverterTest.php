804c38ab37df73afc39066782d3ccb750fe40b51
ito ryo <electrooculogram@gmail.com>
2025-01-11 20:56:19 +0900

    update tests (red)

--- tests/Core/NumberConverterTest.php
+++ tests/Core/NumberConverterTest.php
@@ -16,5 +16,6 @@ class NumberConverterTest extends TestCase
         $this->assertEquals('Fizz', $fizzbuzz->convert(3));
         $this->assertEquals('4', $fizzbuzz->convert(4));
         $this->assertEquals('Buzz', $fizzbuzz->convert(5));
+        $this->assertEquals('FizzBuzz', $fizzbuzz->convert(15));
     }
 }
