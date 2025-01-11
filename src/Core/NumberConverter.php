3ae5445eeb73fc791563f65f5da4fb1f3b8e58b8
ito ryo <electrooculogram@gmail.com>
2025-01-11 21:01:55 +0900

    refactor

--- src/Core/NumberConverter.php
+++ src/Core/NumberConverter.php
@@ -5,12 +5,10 @@ namespace Itxryx\FizzBuzz\Core;
 
 class NumberConverter {
     public function convert(int $n): string {
-        if ($n % 3 === 0) {
-            if ($n % 5 === 0) {
-                return "FizzBuzz";
-            } else {
-                return "Fizz";
-            }
+        if ($n % 3 === 0 && $n % 5 === 0) {
+            return "FizzBuzz";
+        } else if ($n % 3 === 0) {
+            return "Fizz";
         } else if ($n % 5 === 0) {
             return "Buzz";
         } else {
